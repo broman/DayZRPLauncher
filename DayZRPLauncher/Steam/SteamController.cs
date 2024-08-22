e74a130c7a65a9b328865ab3eb369a1032d8c6a7
Ryan Broman <ryanjbroman@gmail.com>
2024-08-21 21:59:58 -0700

    steam api bullshit

--- DayZRPLauncher/Steam/SteamController.cs
+++ DayZRPLauncher/Steam/SteamController.cs
@@ -10,6 +10,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Windows;
+using DayZRPLauncher.Steam;
+using System.Text.Json;
+using System.Net;
 
 namespace DayZRPLauncher {
     internal class SteamController {
@@ -17,11 +20,11 @@ namespace DayZRPLauncher {
         ///  There does exist a few Steam libraries, but handling Auth and whatnot is completely undocumented
         ///  in these libraries and are more intended for gamedevs than for interfacing with Steam.
         ///  
-        ///  This class uses undocumented (officially) API endpoints to validate mods
+        ///  This class uses published API endpoints to validate installed mod timestamp == last updated
         /// </summary>
-        static readonly string BASE_URL = "https://api.steampowered.com/";
+        static readonly string BASE_URL = "https://api.steampowered.com";
         static readonly string? API_KEY = MainWindow.parameters.Params.FirstOrDefault(x => x.Label == Constants.API_KEY)?.Value as string;
-        static readonly HttpClient client = new HttpClient();
+        static readonly HttpClient client = new();
         public static SteamController Instance { get; private set; } = new SteamController();
 
 
@@ -29,27 +32,11 @@ namespace DayZRPLauncher {
 
         }
 
-        public string GetUrl(string iface, string endpoint) {
-            return $"{BASE_URL}/{iface}/{endpoint}/v1/?key={API_KEY}";
-        }
-        
-        public string BuildCollectionUrl(string iface, string endpoint, ulong id) {
-            var bse = $"{BASE_URL}/{iface}/{endpoint}/v1/?key={API_KEY}";
-            return $"{bse}&collectioncount=1&publishedfileids%5B0%5D={id}";
-        }
-
-        public string BuildItemUrl(string iface, string endpoint, ulong id) {
-
-        }
-
-        public async Task<string> GetCollectionDetails(ulong id) {
-            var url = $"{BuildCollectionUrl("ISteamRemoteStorage", "GetCollectionDetails", id)}";
-            var resp = await client.PostAsync(GetUrl("ISteamRemoteStorage", "GetCollectionDetails"), null);
-            return await resp.Content.ReadAsStringAsync();
-        }
-
         public async void VerifyMods() {
-            MessageBox.Show(await GetCollectionDetails(Constants.DRP_MODLIST_ID));
+            // I spent 3 hours trying to work with the Steam API tonight. I give up.
+
+            // insert working code here
+
         }
     }
 }
---
+++ DayZRPLauncher/Steam/SteamResponses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayZRPLauncher.Steam
+{
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+
+    public class WorkshopCollectionItem {
+        [JsonPropertyName("publishedfileid")]
+        public string? PublishedFileId { get; set; }
+
+        [JsonPropertyName("sortorder")]
+        public int? SortOrder { get; set; }
+
+        [JsonPropertyName("filetype")]
+        public int? FileType { get; set; }
+    }
+
+    public class CollectionDetail {
+        [JsonPropertyName("children")]
+        public List<WorkshopCollectionItem>? Children { get; set; }
+    }
+
+    public class Response {
+        [JsonPropertyName("collectiondetails")]
+        public List<CollectionDetail>? CollectionDetails { get; set; }
+    }
+
+    public class Root {
+        [JsonPropertyName("response")]
+        public Response? Response { get; set; }
+    }
+}
