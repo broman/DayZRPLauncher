0d00ea2351f5de39ea73565832abfa862ec66607
Ryan Broman <ryanjbroman@gmail.com>
2024-08-22 00:11:09 -0700

    reeeeeeeeeeeeeeeeeee

--- DayZRPLauncher/Steam/SteamController.cs
+++ DayZRPLauncher/Steam/SteamController.cs
@@ -15,6 +15,35 @@ using System.Text.Json;
 using System.Net;
 
 namespace DayZRPLauncher {
+
+    public class LoggingHandler :DelegatingHandler {
+        public LoggingHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler) {
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+            MessageBox.Show(request.ToString());
+            if(request.Content != null) {
+                MessageBox.Show(await request.Content.ReadAsStringAsync());
+            }
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            MessageBox.Show("Here "+response.ToString());
+            /*
+            if(response.Content != null) {
+                MessageBox.Show(await response.Content.ReadAsStringAsync());
+            }*/
+            MessageBox.Show(response.Content.ToString());
+            MessageBox.Show(await response.Content.ReadAsStringAsync());
+
+
+            return response;
+        }
+    }
+
+
+
     internal class SteamController {
         /// <summary>
         ///  There does exist a few Steam libraries, but handling Auth and whatnot is completely undocumented
@@ -36,6 +65,39 @@ namespace DayZRPLauncher {
             // I spent 3 hours trying to work with the Steam API tonight. I give up.
 
             // insert working code here
+
+            using var formContent = new MultipartFormDataContent
+{
+        { new StringContent("1"), "itemcount" },
+        { new StringContent("1599372051"), "publishedfileids%5B0%5D"}
+    };
+
+            var request = new HttpRequestMessage {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri($"{BASE_URL}/ISteamRemoteStorage/GetCollectionDetails/v1/?key=XXX&input_json={{\"itemcount:1,publishedfileids[0]:[1599372051]\"}}"),
+                Content = formContent,
+            };
+
+
+
+            try {
+                using HttpClient client = new HttpClient(new LoggingHandler(new HttpClientHandler()));
+
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                // Ensure successful response
+                response.EnsureSuccessStatusCode();
+
+                // Read and process the response if needed
+                string responseBody = await response.Content.ReadAsStringAsync();
+                //MessageBox.Show(responseBody);
+            }
+            catch(HttpRequestException e) {
+                MessageBox.Show($"Request error: {e.Message}");
+            }
+            catch(Exception e) {
+                MessageBox.Show($"General error: {e.Message}");
+            }
 
         }
     }
